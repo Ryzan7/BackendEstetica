8b0e08d7ec41e976aeefcaaa7d53be6821a93d19
Laís <113736391+laisdiasdev@users.noreply.github.com>
2025-06-26 15:38:55 -0300

    alterado o retorno do método cadastrar para usar status 201 (CREATED)

--- src/main/java/com/esc2/api/estetica/controllers/ProfissionalController.java
+++ src/main/java/com/esc2/api/estetica/controllers/ProfissionalController.java
@@ -5,6 +5,7 @@ import com.esc2.api.estetica.models.ProfissionalModel;
 import com.esc2.api.estetica.services.ProfissionalServiceAPI;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,8 +22,8 @@ public class ProfissionalController {
 
     @PostMapping
     public ResponseEntity<ProfissionalModel> cadastrar(@RequestBody @Valid ProfissionalRecordDto dto) {
-        var profissional = profissionalService.cadastrar(dto);
-        return ResponseEntity.ok(profissional);
+        ProfissionalModel profissionalModel = profissionalService.cadastrar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(profissionalModel);
     }
 
     @GetMapping
@@ -37,14 +38,18 @@ public class ProfissionalController {
 
     @GetMapping("/{id}")
     public ResponseEntity<ProfissionalModel> buscarPorId(@PathVariable UUID id) {
-        Optional<ProfissionalModel> profissional = profissionalService.buscarPorId(id);
-        return profissional.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+        return profissionalService.buscarPorId(id)
+        		.map(ResponseEntity::ok)
+        		.orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProfissionalModel> atualizar(@PathVariable UUID id, @RequestBody @Valid ProfissionalRecordDto dto) {
-        var model = profissionalService.buscarPorId(id).orElseThrow(() -> new RuntimeException("Profissional não encontrado"));
-        var atualizado = profissionalService.atualizar(dto, model);
+    public ResponseEntity<ProfissionalModel> atualizar(
+    		@PathVariable UUID id,
+    		@RequestBody @Valid ProfissionalRecordDto dto) {
+        ProfissionalModel profissionalModel = profissionalService.buscarPorId(id)
+        		.orElseThrow(() -> new RuntimeException("Profissional não encontrado."));
+        ProfissionalModel atualizado = profissionalService.atualizar(dto, profissionalModel);
         return ResponseEntity.ok(atualizado);
     }
 
